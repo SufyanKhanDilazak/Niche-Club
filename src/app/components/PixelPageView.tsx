7f8472cd4e4edb9044ee825baeccd4408f150ea7
SufyanKhanDilazak <sufyank48@hotmail.com>
2025-12-16 01:03:59 +0500

    Deploy: production update

--- src/app/components/PixelPageView.tsx
+++ src/app/components/PixelPageView.tsx
@@ -1,17 +1,16 @@
 "use client";
 
-import { usePathname, useSearchParams } from "next/navigation";
+import { usePathname } from "next/navigation";
 import { useEffect } from "react";
 
 export default function PixelPageView() {
   const pathname = usePathname();
-  const searchParams = useSearchParams();
 
   useEffect(() => {
     if (typeof window !== "undefined" && typeof window.fbq === "function") {
       window.fbq("track", "PageView");
     }
-  }, [pathname, searchParams]);
+  }, [pathname]);
 
   return null;
 }
